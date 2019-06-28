e3ae7862adcb4048e1d5fa313810381b03c22211
Shinya Hijiri <shinya.hijiri@gmail.com>
2019-06-28 12:20:56 +0900

    JSON返却するREST APIを作成。試行錯誤のゴミ残ったまま。

--- src/test/java/com/example/EmbeddedServletStartTest.java
+++ src/test/java/com/example/EmbeddedServletStartTest.java
@@ -1,8 +1,12 @@
 package com.example;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.apache.catalina.core.StandardContext;
 import org.apache.catalina.startup.Tomcat;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -14,6 +18,9 @@ import java.nio.charset.StandardCharsets;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class EmbeddedServletStartTest {
+
+    @Autowired
+    RestTemplate restTemplate;
 
     @Test
     void test() throws Exception {
@@ -29,11 +36,14 @@ public class EmbeddedServletStartTest {
         tomcat.start();
 //        tomcat.getServer().await();
 
-        HttpURLConnection conn = (HttpURLConnection) URI.create("http://localhost:8080/hello").toURL().openConnection();
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8))) {
-            assertEquals("Hello Servlet!!", reader.readLine());
-        }
-        conn.disconnect();
+        Response response = RestAssured.get("/hello");
+        System.out.println(response.asString());
+
+//        HttpURLConnection conn = (HttpURLConnection) URI.create("http://localhost:8080/hello").toURL().openConnection();
+//        try (BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8))) {
+//            assertEquals("Hello Servlet!!", reader.readLine());
+//        }
+//        conn.disconnect();
 
         tomcat.stop();
     }
